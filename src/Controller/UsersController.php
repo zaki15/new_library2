d45cef2f022c912109115e8f2d11d16344af3092
yusai <yusaiokabe@gmail.com>
2018-05-22 11:09:21 +0900

    test
    test

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -52,7 +52,8 @@ class UsersController extends AppController
         if($this->request->is('post')){
 
 
-          
+
+
 
           $query = $this->request->data['Users']['query'];
           $condition = ['conditions'=>['id'=>$query]];
@@ -75,11 +76,11 @@ class UsersController extends AppController
         if ($this->request->is('post')) {
             $user = $this->Users->patchEntity($user, $this->request->getData());
             if ($this->Users->save($user)) {
-                //$this->Flash->success(__('The user has been saved.'));
+                $this->Flash->success(__('The user has been saved.'));
 
                 return $this->redirect(['action' => 'index']);
             }
-            //$this->Flash->error(__('The user could not be saved. Please, try again.'));
+            $this->Flash->error(__('The user could not be saved. Please, try again.'));
         }
         $this->set(compact('user'));
     }
