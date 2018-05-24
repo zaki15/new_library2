e2d24e38402c7e9786ed423696900f61da8380cd
tani-naigai <39504768+tani-naigai@users.noreply.github.com>
2018-05-24 14:55:35 +0900

    tani
    14:55

--- src/Controller/BookstatesController.php
+++ src/Controller/BookstatesController.php
@@ -58,69 +58,74 @@ class BookstatesController extends AppController
      *
      * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
      */
-    public function add()
-    {
-        $bookstate = $this->Bookstates->newEntity();
+     public function add()
+     {
+       $bookstate = $this->Bookstates->newEntity();
 
-  /* @okabe l66~76*/
-        /*$books = $this->Books->newEntity();
-        if ($this->request->isPost()) {
-            $book_id = $this->requestdata[''][''];
-            $bookstate_entity =['book_id'=>$book_id,'arrival_date'=>$arr_date];
-            $bookstate = $this->Bookstates->patchEntity($bookstate, $this->request->getData());
-            $book = $this->Books->patchEntity($books, $this->request->getData());
-            if ($this->Bookstates->save($bookstate) && $this->Books->save($books)) {*/
+       /* @okabe l66~76*/
+       /*$books = $this->Books->newEntity();
+       if ($this->request->isPost()) {
+       $book_id = $this->requestdata[''][''];
+       $bookstate_entity =['book_id'=>$book_id,'arrival_date'=>$arr_date];
+       $bookstate = $this->Bookstates->patchEntity($bookstate, $this->request->getData());
+       $book = $this->Books->patchEntity($books, $this->request->getData());
+       if ($this->Bookstates->save($bookstate) && $this->Books->save($books)) {*/
 
-            $book = $this->Books->newEntity();
-            if ($this->request->isPost()) {
-              $isbn = $this->request->data['Bookstates']['isbn'];
+       $book = $this->Books->newEntity();
+       if ($this->request->isPost()) {
+         $isbn = $this->request->data['Bookstates']['isbn'];
+         $book_ids=$this->Books->find('all',['conditions' => ['isbn' => $isbn]]);
+         foreach ($book_ids as $book_ids) {
+           $book_id= $book_ids->id;
+         }
 
-              if (empty($book_isbn = $this->Books->find('all',['conditions' => ['isbn' => $isbn]]))) {
-                //ISBN番号がない場合の処理→bookstatesとbooksに値を保存
-                $isbn = $this->request->data['Bookstates']['isbn'];
-                $category_id = $this->request->data['Bookstates']['category_id'];
-                $name = $this->request->data['Bookstates']['name'];
-                $author = $this->request->data['Bookstates']['author'];
-                $publisher = $this->request->data['Bookstates']['publisher'];
-                $publish_date = $this->request->data['Bookstates']['publish_date'];
-                $book_entity =['isbn'=>$isbn,'category_id'=>$category_id,'name'=>$name,'author'=>$author,'publisher'=>$publisher,'publish_date'=>$publish_date];
+         if (empty($book_id)) {
+           //ISBN番号がない場合の処理→bookstatesとbooksに値を保存
+           $isbn = $this->request->data['Bookstates']['isbn'];
+           $category_id = $this->request->data['Bookstates']['category_id'];
+           $name = $this->request->data['Bookstates']['name'];
+           $author = $this->request->data['Bookstates']['author'];
+           $publisher = $this->request->data['Bookstates']['publisher'];
+           $publish_date = $this->request->data['Bookstates']['publish_date'];
+           $book_entity =['isbn'=>$isbn,'category_id'=>$category_id,'name'=>$name,'author'=>$author,'publisher'=>$publisher,'publish_date'=>$publish_date];
 
-                $arrival_date = $this->request->data['Bookstates']['arrival_date'];
-                $delete_date = $this->request->data['Bookstates']['delete_date'];
-                $state = $this->request->data['Bookstates']['state'];
-                $bookstate_entity =['arrival_date'=>$arrival_date,'delete_date'=>$delete_date,'state'=>$state];
+           $arrival_date = $this->request->data['Bookstates']['arrival_date'];
+           $delete_date = $this->request->data['Bookstates']['delete_date'];
+           $state = $this->request->data['Bookstates']['state'];
+           $bookstate_entity =['arrival_date'=>$arrival_date,'delete_date'=>$delete_date,'state'=>$state];
 
-                $book = $this->Books->newEntity();
-                $bookstate = $this->Bookstates->newEntity();
-                $book = $this->Books->patchEntity($book, $book_entity);
-                $bookstate = $this->Bookstates->patchEntity($bookstate, $bookstate_entity);
+           $book = $this->Books->newEntity();
+           $bookstate = $this->Bookstates->newEntity();
+           $book = $this->Books->patchEntity($book, $book_entity);
+           $bookstate = $this->Bookstates->patchEntity($bookstate, $bookstate_entity);
 
-                if ($this->Books->save($book) && $this->Bookstates->save($bookstate)) {
-                  $this->Flash->success(__('bookとstateに書き込みました'));
-                  return $this->redirect(['action' => 'index']);
-                }
-                $bookstate = $this->request->data['Bookstates']['delete_date'];
-              }else {
-                //ISBN番号がある場合の処理→bookstatesだけに値を保存
-                $arrival_date = $this->request->data['Bookstates']['arrival_date'];
-                $delete_date = $this->request->data['Bookstates']['delete_date'];
-                $state = $this->request->data['Bookstates']['state'];
-                
-                $bookstate_entity =['arrival_date'=>$arrival_date,'delete_date'=>$delete_date,'state'=>$state];
-                $bookstate = $this->Bookstates->patchEntity($bookstate, $bookstate_entity);
+           if ($this->Books->save($book) && $this->Bookstates->save($bookstate)) {
+             $this->Flash->success(__('bookとstateに書き込みました'));
+             return $this->redirect(['action' => 'index']);
+           }
+           $bookstate = $this->request->data['Bookstates']['delete_date'];
+         }else {
+           //ISBN番号がある場合の処理→bookstatesだけに値を保存
 
-                if ($this->Bookstates->save($bookstate)) {
-                  $this->Flash->success(__('bookstateだけに書き込みました'));
-                  return $this->redirect(['action' => 'index']);
-                }
-              }
-            }
-            // $books = $this->Bookstates->Books->find('list', ['limit' => 200]);
-            // $categories = $this->Books->Categories->find('list', ['limit' => 200]);
-            // $publishers = $this->Books->Publishers->find('list', ['limit' => 200]);
+           $arrival_date = $this->request->data['Bookstates']['arrival_date'];
+           $delete_date = $this->request->data['Bookstates']['delete_date'];
+           $state = $this->request->data['Bookstates']['state'];
 
-            $this->set(compact('bookstate','book'));
-          }
+           $bookstate_entity =['book_id'=>$book_id,'arrival_date'=>$arrival_date,'delete_date'=>$delete_date,'state'=>$state];
+           $bookstate = $this->Bookstates->patchEntity($bookstate, $bookstate_entity);
+
+           if ($this->Bookstates->save($bookstate)) {
+             $this->Flash->success(__('bookstateだけに書き込みました'));
+             return $this->redirect(['action' => 'index']);
+           }
+         }
+       }
+       // $books = $this->Bookstates->Books->find('list', ['limit' => 200]);
+       // $categories = $this->Books->Categories->find('list', ['limit' => 200]);
+       // $publishers = $this->Books->Publishers->find('list', ['limit' => 200]);
+
+       $this->set(compact('bookstate','book'));
+     }
 
     /**
      * Edit method
--- src/Template/Bookstates/add.ctp
+++ src/Template/Bookstates/add.ctp
@@ -18,7 +18,7 @@
         echo '<tr><td>区分</td><td>'.$this->Form->select('Bookstates.category_id', [0,1, 2, 3, 4, 5,6,7,8,9]).'</td></tr>';
         echo '<tr><td>タイトル</td><td>'.$this->Form->control('Bookstates.name',['label'=>'']).'</td></tr>';
         echo '<tr><td>著者名</td><td>'.$this->Form->control('Bookstates.author',['label'=>'']).'</td></tr>';
-        echo '<tr><td>出版社名</td><td>'.$this->Form->control('Bookstates.publisher',['label'=>'']).'</td></tr>';
+        echo '<tr><td>出版社コード</td><td>'.$this->Form->control('Bookstates.publisher',['label'=>'']).'</td></tr>';
         echo '<tr><td>出版日</td><td>'.$this->Form->control('Bookstates.publish_date',['label'=>'']).'</td></tr>';
         echo '<tr><td>入荷年月日</td><td>'.$this->Form->control('Bookstates.arrival_date',['label'=>'']).'</td></tr>';
         echo '<tr><td>廃棄年月日</td><td>'.$this->Form->control('Bookstates.delete_date',['label'=>'']).'</td></tr>';
@@ -35,41 +35,3 @@
 
   <button class="under_button">情報検索画面へ</button>
 </div>
-
-
-
-
-<!--
-<nav class="large-3 medium-4 columns" id="actions-sidebar">
-    <ul class="side-nav">
-        <li class="heading"><?= __('Actions') ?></li>
-        <li><?= $this->Html->link(__('List Bookstates'), ['action' => 'index']) ?></li>
-        <li><?= $this->Html->link(__('List Books'), ['controller' => 'Books', 'action' => 'index']) ?></li>
-        <li><?= $this->Html->link(__('New Book'), ['controller' => 'Books', 'action' => 'add']) ?></li>
-        <li><?= $this->Html->link(__('List Rentals'), ['controller' => 'Rentals', 'action' => 'index']) ?></li>
-        <li><?= $this->Html->link(__('New Rental'), ['controller' => 'Rentals', 'action' => 'add']) ?></li>
-        <li><?= $this->Html->link(__('List Reservations'), ['controller' => 'Reservations', 'action' => 'index']) ?></li>
-        <li><?= $this->Html->link(__('New Reservation'), ['controller' => 'Reservations', 'action' => 'add']) ?></li>
-    </ul>
-</nav>
-<div class="bookstates form large-9 medium-8 columns content">
-    <?= $this->Form->create($bookstate) ?>
-    <fieldset>
-        <legend><?= __('Add Bookstate') ?></legend>
-        <?php
-            echo $this->Form->control('book_id', ['options' => $books]);
-            echo $this->Form->control('ISBN番号');
-            echo $this->Form->control('区分');
-            echo $this->Form->control('タイトル');
-            echo $this->Form->control('著者名');
-            echo $this->Form->control('出版社名');
-            echo $this->Form->control('出版日');
-            echo $this->Form->control('資料ID');
-            echo $this->Form->control('入荷年月日');
-            echo $this->Form->control('廃棄年月日');
-            echo $this->Form->control('蔵書冊数');
-        ?>
-    </fieldset>
-    <?= $this->Form->button(__('Submit')) ?>
-    <?= $this->Form->end() ?>
-</div> -->
